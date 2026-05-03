73d374eec0e4b4d72d795797a3d1e1097fde7cf6
Imran Al Farabi <imranalfarabidevworks@gmail.com>
2026-05-04 00:47:19 +0600

    added render link

--- src/lib/auth-client.js
+++ src/lib/auth-client.js
@@ -3,5 +3,4 @@ import { createAuthClient } from "better-auth/react";
 export const authClient = createAuthClient({
   baseURL: process.env.NEXT_PUBLIC_BETTER_AUTH_URL ||"https://gallery-of-tiles.vercel.app",
 });
-
 export const { signIn, signOut, signUp, useSession, getSession } = authClient;
